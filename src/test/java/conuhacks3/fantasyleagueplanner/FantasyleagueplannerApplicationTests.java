62de91b2283f0b58f7e5cb104ec3423455dd0f9a
Philippe Li <philo1196@gmail.com>
2018-01-27 10:57:20 -0500

    initial commit

---
+++ src/test/java/conuhacks3/fantasyleagueplanner/FantasyleagueplannerApplicationTests.java
@@ -0,0 +1,16 @@
+package conuhacks3.fantasyleagueplanner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FantasyleagueplannerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
