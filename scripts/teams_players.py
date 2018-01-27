b7f2e31cd20f911345e628edab74787c59a8701e
patrickspensieri <patrick.spensieri@gmail.com>
2018-01-27 18:54:30 -0500

    read csv into pandas dataframe instead of json

--- scripts/teams_players.py
+++ scripts/teams_players.py
@@ -1,22 +1,35 @@
 from urllib2 import Request, urlopen
 import json
+import requests
+import numpy as np
 from pandas.io.json import json_normalize
+from pandas import read_csv
 
-data = []
-for teamId in range(1, 31):
+
+teams = read_csv('http://conu.astuce.media:9993/api/sports/hockey/teams?format=csv&Id='+str(1))
+players = read_csv('http://conu.astuce.media:9993/api/sports/hockey/team/persons?format=csv&TeamId='+str(1))
+for teamId in range(2, 31):
+    teams = teams.append(read_csv('http://conu.astuce.media:9993/api/sports/hockey/teams?format=csv&Id='+str(teamId)))
+    players = players.append(read_csv('http://conu.astuce.media:9993/api/sports/hockey/team/persons?format=csv&TeamId='+str(teamId)))
+print(teams.head())
+print(players.head())
+
+
+# data = []
+# for teamId in range(1, 2):
     # get all 31 teams
-    req = Request('http://conu.astuce.media:9993/api/sports/hockey/teams?Id='+str(teamId))
-    res = urlopen(req)
-    elevations = res.read()
+    # req = Request('http://conu.astuce.media:9993/api/sports/hockey/teams?Id='+str(teamId))
+    # res = urlopen(req)
+    # elevations = res.read()
     # data is of type list
-    data.append(json.loads(elevations))
+    # data.append(json.loads(elevations))
     # get all players for team
-    req = Request('http://conu.astuce.media:9993/api/sports/hockey/team/persons?TeamId='+str(teamId))
-    res = urlopen(req)
-    elevations = res.read()
-    data[teamId-1].append(json.loads(elevations))
+    # req = Request('http://conu.astuce.media:9993/api/sports/hockey/team/persons?TeamId='+str(teamId))
+    # res = urlopen(req)
+    # elevations = res.read()
+    # data[teamId-1].append(json.loads(elevations))
 
-with open('teams_players.json', 'w') as outfile:
-    json.dump(data, outfile)
-    
+# with open('teams_players.json', 'w') as outfile:
+#     json.dump(data, outfile)
+#
 # print(json_normalize(data['results']))
