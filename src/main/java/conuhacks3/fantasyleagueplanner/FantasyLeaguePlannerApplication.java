2e5288174e31239cb91bceba223e0c1c875cb085
Philippe Li <philo1196@gmail.com>
2018-01-28 10:19:29 -0500

    fixed sorting for soccer players

--- src/main/java/conuhacks3/fantasyleagueplanner/FantasyLeaguePlannerApplication.java
+++ src/main/java/conuhacks3/fantasyleagueplanner/FantasyLeaguePlannerApplication.java
@@ -137,7 +137,23 @@ public class FantasyLeaguePlannerApplication {
 			SoccerPlayer currentPlayer = players.get(i);
 			playersMap.put(currentPlayer.getFullName(), currentPlayer.getScore());
 		}
-		return gson.toJson(playersMap);
+
+		return gson.toJson(FantasyLeaguePlannerApplication.sortByValue(playersMap));
+    }
+
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+        List<Map.Entry<K, V>> list = new LinkedList<Map.Entry<K, V>>(map.entrySet());
+        Collections.sort( list, new Comparator<Map.Entry<K, V>>() {
+            public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+                return (o2.getValue()).compareTo(o1.getValue());
+            }
+        });
+
+        Map<K, V> result = new LinkedHashMap<K, V>();
+        for (Map.Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
     }
 
 	public static void main(String[] args) {
