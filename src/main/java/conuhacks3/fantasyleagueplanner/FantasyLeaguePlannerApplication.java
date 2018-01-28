cbc68378559a9b18ae0196d4c0266fd184c772e7
Philippe Li <philo1196@gmail.com>
2018-01-27 20:39:30 -0500

    created endpoint for pool config

--- src/main/java/conuhacks3/fantasyleagueplanner/FantasyLeaguePlannerApplication.java
+++ src/main/java/conuhacks3/fantasyleagueplanner/FantasyLeaguePlannerApplication.java
@@ -4,7 +4,9 @@ import com.google.gson.Gson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
@@ -32,6 +34,15 @@ public class FantasyLeaguePlannerApplication {
         map.put("test key", "test value");
         return gson.toJson(map);
     }
+
+    @RequestMapping(value = "/post/poolConfig", method = RequestMethod.POST)
+	@ResponseBody
+	public String poolConfig(@RequestBody String json) {
+		Gson gson = new Gson();
+		PoolConfiguration poolConfiguration = gson.fromJson(json, PoolConfiguration.class);
+
+		return "{}"; // TODO return json of top 10 players
+	}
 
 
 	public static void main(String[] args) {
